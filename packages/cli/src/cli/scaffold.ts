9eab91188f36c0a80cbde62fb8bd2c7eefcdd616
Harsh Thakur <178945966+devharshthakur@users.noreply.github.com>
2025-06-12 19:19:35 +0530

    Add comments in scaffold file

--- packages/cli/src/cli/scaffold.ts
+++ packages/cli/src/cli/scaffold.ts
@@ -5,12 +5,13 @@ import { GitInjector } from '@/injectors/git/index.js';
 
 export async function scaffoldProject(options: ProjectOptions) {
   const { folders, packageManager, projectName, moduleSystem, initGit, installDeps } = options;
+  // Create injectors instances
   const baseInjector = new BaseInjector(folders, packageManager, projectName, moduleSystem, initGit);
-  await baseInjector.inject();
-
   const gitInjector = new GitInjector(initGit);
-  await gitInjector.inject();
-
+  // Call all the injectors instances
+  await baseInjector.inject();
+  await gitInjector.inject(); // always inject git at last so all files get commited (check its index.ts file)
+  // When all injections are completed then install the dependencies
   if (installDeps) {
     try {
       await execa(packageManager, ['install'], { stdio: 'inherit' });
